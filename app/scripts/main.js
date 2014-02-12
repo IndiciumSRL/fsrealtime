f7d8bd6deb1fbd37157437cc65c6495974a27d13
Joao Mesquita <jmesquita@indicium.com.ar>
2014-02-12 00:10:20 -0300

    On the way of adding features to the view.

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -9,6 +9,7 @@ window.fsrealtime = {
     init: function () {
         'use strict';
         fsrealtime.console = new fsrealtime.Views.ConsoleView();
+        fsrealtime.console.addLogLine({log: 'Test', level: 0});
     }
 };
 
--- app/scripts/views/console.js
+++ app/scripts/views/console.js
@@ -9,6 +9,8 @@ fsrealtime.Views = fsrealtime.Views || {};
 
         template: JST['app/scripts/templates/console.ejs'],
         el: 'div.content .col-xs-11',
+        levels: ['console', 'aler', 'crit', 'err', 'warning', 'notice', 'info', 'debug'],
+        currentLevel : 7,
 
         initialize: function() {
         	this.render();
@@ -16,6 +18,13 @@ fsrealtime.Views = fsrealtime.Views || {};
         render: function() {
         	this.$el.html(this.template());
         	return this;
+        },
+        addLogLine: function(logline) {
+        	var $span = $('<span/>').html(logline.log).addClass(this.levels[logline.level]);
+        	this.$('div.console').append($span);
+        },
+        setLevel: function(level) {
+        	this.currentLevel = level;
         }
 
     });
